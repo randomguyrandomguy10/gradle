da2c3fabed6a7d102866b636d1915d7f70eeaedd
kowaipanda <randomguyrandomguy10@gmail.com>
2026-04-08 14:39:38 +0530

    add the controller

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello";
+        }
+}
